7e25a2c1fcf3679f2e4468e404e17e4f29d73017
WDRK <lllrrrlrssr@gmail.com>
2021-05-10 14:19:53 +0900

    Contents.js에서 covid19api.com의 정보를 받아오고 콘솔에 출력함

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #### React로 만든 코로나 현황 집계 사이트입니다
 
 #### [code Scalper](https://www.youtube.com/channel/UC1wWTimSew9rYzEZRVYVlbg)의 [리액트를 몰라도 무작정 따라 해보는 리액트 코로나 집계 사이트](https://youtu.be/DtLhiMxgsm0)를 학습한 저장소입니다.
+
+#### [covid19api](https://covid19api.com/)의 API를 사용합니다.
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Contents = () => {
+  // 클래스에서 마운트가 됐을 때 바로 메서드를 실행하는 효과를 위해 useEffect를 사용함
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get(
+        'https://api.covid19api.com/total/dayone/country/kr'
+      );
+      console.log(res);
+    };
+    fetchEvents();
+  });
   return (
     <section>
       <h2>국내 코로나 현황</h2>
